1de0b28f21aba3ade3ac5ca31767a84d06897024
tgavri <thom54s8@stud.kea.dk>
2024-11-27 09:18:00 +0100

    Initial commit.

---
+++ src/main/java/kea/guessr/GuessrApplication.java
@@ -0,0 +1,13 @@
+package kea.guessr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessrApplication.class, args);
+    }
+
+}

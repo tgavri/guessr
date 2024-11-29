cddb84b29b7d364315e05f8ad14e232597517e9d
tgavri <thom54s8@stud.kea.dk>
2024-11-29 03:49:02 +0100

    Index, fragments, about, admin(udkast), privacy. Boys plz kun lav sider på ENGELSK, så vi har en rød tråd.

---
+++ src/main/java/kea/guessr/PageController.java
@@ -0,0 +1,36 @@
+package kea.guessr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String showIndexPage() {
+        return "index";
+    }
+    @GetMapping("/about")
+    public String showAboutPage() {
+        return "about";
+    }
+
+    @GetMapping("/privacy")
+    public String showPrivacyPage() {
+        return "privacy";
+    }
+    @GetMapping("/admin")
+    public String showAdminPage() {
+        return "admin";
+    }
+    @GetMapping("/fragments/navbar")
+    public String getIndexHeader() {
+        return "fragments/navbar";
+    }
+
+    @GetMapping("/fragments/footer")
+    public String getIndexFooter() {
+        return "fragments/footer";
+    }
+
+
+}
